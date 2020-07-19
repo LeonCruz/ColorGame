99525dbfdeba40821e52c7d2b28bb378c05fbec4
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-07-19 20:42:44 -0300

    Uso de databind e navigation

--- app/src/main/java/br/com/leon/colorgame/GameFragment.kt
+++ app/src/main/java/br/com/leon/colorgame/GameFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import br.com.leon.colorgame.databinding.FragmentGameBinding
 import kotlinx.android.synthetic.main.fragment_game.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,44 +22,49 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        btnRed.setBackgroundColor(Color.RED)
-        btnBlue.setBackgroundColor(Color.BLUE)
-        btnGreen.setBackgroundColor(Color.GREEN)
-        btnYellow.setBackgroundColor(Color.YELLOW)
+        val binding = DataBindingUtil.inflate<FragmentGameBinding>(
+            inflater, R.layout.fragment_game, container, false
+        )
+        binding.btnRed.setBackgroundColor(Color.RED)
+        binding.btnBlue.setBackgroundColor(Color.BLUE)
+        binding.btnGreen.setBackgroundColor(Color.GREEN)
+        binding.btnYellow.setBackgroundColor(Color.YELLOW)
 
-        btnRed.setOnClickListener {
+        binding.btnRed.setOnClickListener {
             if (Game.compareColors(txtColor.currentTextColor, btnRed.text.toString())) updateScore() else gameOver()
             changeValues()
         }
-        btnYellow.setOnClickListener {
+        binding.btnYellow.setOnClickListener {
             if (Game.compareColors(txtColor.currentTextColor, btnYellow.text.toString())) updateScore() else gameOver()
             txtColor.setTextColor(Game.changeTextColor())
             txtColor
         }
-        btnGreen.setOnClickListener {
+        binding.btnGreen.setOnClickListener {
             if (Game.compareColors(txtColor.currentTextColor, btnGreen.text.toString())) updateScore() else gameOver()
             changeValues()
         }
-        btnBlue.setOnClickListener {
+        binding.btnBlue.setOnClickListener {
             if (Game.compareColors(txtColor.currentTextColor, btnBlue.text.toString())) updateScore() else gameOver()
             changeValues()
         }
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        return binding.root
     }
 
     private fun changeValues() {
-        txtColor.setTextColor(Game.changeTextColor())
-        txtColor.text = Game.changeText()
+        binding.txtColor.setTextColor(Game.changeTextColor())
+        binding.txtColor.text = Game.changeText()
     }
 
     private fun updateScore() {
-        val value = score.text.toString().toInt()
-        score.text = (value + 1).toString()
+        val value = binding.score.text.toString().toInt()
+        binding.score.text = (value + 1).toString()
     }
 
     private fun gameOver() {
